9b1c5c33071c193aee5c8725d05399430a7a2f03
Raghav Agrawal <raghavagr2024@gmail.com>
2021-10-04 20:26:28 -0500

    added comment (trial)

--- app/src/main/java/com/example/science_fair/MainActivity.java
+++ app/src/main/java/com/example/science_fair/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /*wow this comment is so cool*/
+        //e
 
 
 
