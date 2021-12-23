8aa9b52aa6325cf72ccd5e88c708cb38ee3b82a8
Raghav Agrawal <raghavagr2024@gmail.com>
2021-12-22 20:50:11 -0600

    Added an input method for the user and implemented method to submit the text.

--- app/src/main/java/com/example/science_fair/MainActivity.java
+++ app/src/main/java/com/example/science_fair/MainActivity.java
@@ -3,18 +3,34 @@ package com.example.science_fair;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import java.util.*;
+import android.view.View;
+import android.widget.EditText;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    EditText input;
+    String link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*wow this comment is so cool*/
-        //e
 
 
+
+
+    }
+    //used to take input
+    public void process(View view){
+        input = (EditText) findViewById(R.id.text);
+        String s = input.getText().toString();
+        parser(s);
+
+    }
+
+    //parsing the input for the apk
+    public void parser(String s){
 
     }
 }
